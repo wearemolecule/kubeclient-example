41d35b1c42439ddea93b27ff186fff9061de54e2
Stephen Meriwether <smeriwether714@gmail.com>
2016-06-08 16:53:21 -0500

    Fix copyright

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Stephen Meriwether <stephen@molecule.io>
+// Copyright © 2016 Molecule Software <devs@molecule.io>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
